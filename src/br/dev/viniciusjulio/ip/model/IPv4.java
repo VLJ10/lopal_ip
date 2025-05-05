cdc8b251231ff51d44eb470f36244b8ec0ae16c1
VINICIUS JULIO DE OLIVEIRA <25132898@CFP127ALUNO.LOCAL>
2025-05-05 16:51:45 -0300

    Primeiro commit

---
+++ src/br/dev/viniciusjulio/ip/model/IPv4.java
@@ -0,0 +1,18 @@
+package br.dev.viniciusjulio.ip.model;
+
+public class IPv4 {
+	
+	private static int oc1;
+	private static int oc2;
+	private static int oc3;
+	private static int oc4;
+	
+	public String[] ip = new String[5];
+	
+	
+	
+	
+		
+	
+	
+}
